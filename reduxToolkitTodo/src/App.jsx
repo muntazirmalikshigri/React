f1def28e6313e36facff2958e2c86adde8bbbde8
muntazirmalikshigri <muntazirshigri.12@gmail.com>
2024-11-28 02:13:09 +0500

    redux tookit

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>lear redux toolkit</h1>
+     
+     <Todos />
+    </>
+  )
+}
+
+export default App
